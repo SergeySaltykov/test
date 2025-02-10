ab3285fd3ca18aa4ef1b76ae7957674151f4f407
s.saltykov <fermager@yandex.ru>
2025-02-11 00:16:18 +0700

    sass migration @import on @use

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import App from './app/App';
 import { BrowserRouter } from 'react-router';
 import './app/styles/global.css';
 
-
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
